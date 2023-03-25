9e2322282967e9b5d71ab209ffd00c2efcf867f6
Nam Kieu <52618760+knnam69@users.noreply.github.com>
2023-03-25 10:59:33 +0700

    Update install-promagent.sh

--- install-promagent.sh
+++ install-promagent.sh
@@ -8,5 +8,9 @@ cp node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin/
 chown nodeusr:nodeusr /usr/local/bin/node_exporter
 wget https://raw.githubusercontent.com/knnam69/prom-agent/main/node_exporter.service
 cp node_exporter.service /etc/systemd/system/
-systemctl daemon-reload && systemctl enable node_exporter && systemctl start node_exporter && systemctl status node_exporter
-firewall-cmd --permanent --zone=public --add-port=9100/tcp && firewall-cmd --reload
+systemctl daemon-reload
+systemctl enable node_exporter
+systemctl start node_exporter
+systemctl status node_exporter
+firewall-cmd --permanent --zone=public --add-port=9100/tcp
+firewall-cmd --reload
